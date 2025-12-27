8f6afcb296596b4ab48d6ee5a59ac53b88b89162
prachishah0105-ai <prachishah0105@gmail.com>
2025-12-27 16:54:28 +0530

    Initialize Express server with ping-pong endpoint

---
+++ ping-pong/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let counter = 0;
+
+app.get('/pingpong', (req, res) => {
+  res.send(`pong ${counter++}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
